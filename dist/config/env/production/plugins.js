0dbd8a892d3629eba7feacc5d00140d4e1169864
Fedor Sokolov <feojoker@gmail.com>
2023-01-10 12:51:15 +0400

    Add `strapi-plugin-rest-cache` for caching data

--- dist/config/env/production/plugins.js
+++ dist/config/env/production/plugins.js
@@ -13,4 +13,34 @@ module.exports = ({ env }) => ({
             },
         },
     },
+    "rest-cache": {
+        config: {
+            provider: {
+                name: "memory",
+                options: {
+                    max: 32767,
+                    maxAge: 3600,
+                },
+            },
+            strategy: {
+                contentTypes: [
+                    // list of Content-Types UID to cache
+                    "api::default-seo.default-seo",
+                    "api::homepage.homepage",
+                    "api::about-page.about-page",
+                    "api::contact-page.contact-page",
+                    "api::projects-page.projects-page",
+                    {
+                        contentType: "api::language-icon.language-icon",
+                        routes: /* @type {CacheRouteConfig[]} */ [
+                            {
+                                path: '/api/language-icons',
+                                method: 'GET', // can be omitted, defaults to GET
+                            },
+                        ],
+                    }
+                ],
+            },
+        },
+    },
 });
